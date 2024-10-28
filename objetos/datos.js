2ef1eed78fb1d9960b50363c061f995caa70fbc0
mpenaEVG <mauriciopenadominguez.guadalupe@alumnado.fundacionloyola.net>
2024-10-28 19:01:05 +0100

    ejercicio de repaso de arrays

---
+++ objetos/datos.js
@@ -0,0 +1,83 @@
+    const futbolistas = [
+      {
+          nombre: "Lionel Messi",
+          equipo: "Inter Miami",
+          posicion: "Delantero",
+          goles: 40,
+          asistencias: 18,
+          partidosJugados: 30,
+          tarjetaAmarilla: 3,
+          tarjetaRoja: 0
+      },
+      {
+          nombre: "Cristiano Ronaldo",
+          equipo: "Al Nassr",
+          posicion: "Delantero",
+          goles: 35,
+          asistencias: 12,
+          partidosJugados: 28,
+          tarjetaAmarilla: 5,
+          tarjetaRoja: 1
+      },
+      {
+          nombre: "Kevin De Bruyne",
+          equipo: "Manchester City",
+          posicion: "Mediocampista",
+          goles: 10,
+          asistencias: 22,
+          partidosJugados: 25,
+          tarjetaAmarilla: 2,
+          tarjetaRoja: 0
+      },
+      {
+          nombre: "Virgil van Dijk",
+          equipo: "Liverpool",
+          posicion: "Defensa",
+          goles: 5,
+          asistencias: 3,
+          partidosJugados: 27,
+          tarjetaAmarilla: 4,
+          tarjetaRoja: 1
+      },
+      {
+          nombre: "Thibaut Courtois",
+          equipo: "Real Madrid",
+          posicion: "Portero",
+          goles: 0,
+          asistencias: 1,
+          partidosJugados: 29,
+          tarjetaAmarilla: 2,
+          tarjetaRoja: 0
+      },
+      {
+          nombre: "Erling Haaland",
+          equipo: "Manchester City",
+          posicion: "Delantero",
+          goles: 45,
+          asistencias: 5,
+          partidosJugados: 30,
+          tarjetaAmarilla: 3,
+          tarjetaRoja: 0
+      },
+      {
+          nombre: "Kylian Mbappé",
+          equipo: "Paris Saint-Germain",
+          posicion: "Delantero",
+          goles: 42,
+          asistencias: 10,
+          partidosJugados: 29,
+          tarjetaAmarilla: 2,
+          tarjetaRoja: 0
+      },
+      {
+          nombre: "Neymar Jr",
+          equipo: "Al-Hilal",
+          posicion: "Delantero",
+          goles: 20,
+          asistencias: 15,
+          partidosJugados: 26,
+          tarjetaAmarilla: 4,
+          tarjetaRoja: 1
+      }
+];
+
---
+++ objetos/ejercicios.js
@@ -0,0 +1,141 @@
+'use strict'
+
+  golesPromedio(futbolistas)
+  function masDe20Goles(futbolistas){
+      futbolistas.forEach(futbolista =>{
+        if(futbolista.goles > 20){
+          alert(futbolista.nombre)
+        }
+      })
+  }
+
+  function golesTotales(futbolistas){
+      let goles = 0
+      futbolistas.forEach(futbolista =>{
+          goles += futbolista.goles
+      })
+    alert("El total de goles es "+goles)
+  }
+
+  function burbujaAsistencias(futbolistas){
+    let jugadores = []
+    futbolistas.forEach(futbolista =>{
+      jugadores.push({
+        nombre: futbolista.nombre,
+        asistencias: futbolista.asistencias
+    })
+  })
+
+  for(let i=0;i<jugadores.length;i++){
+    for(let j=0;j<jugadores.length-i-1;j++){
+      if(jugadores[j].asistencias<jugadores[j+1].asistencias){
+             let temp = jugadores[j];
+            jugadores[j] = jugadores[j + 1];
+            jugadores[j + 1] = temp;
+      }
+    }
+  }
+  console.log(jugadores)
+  }
+
+  function sortAsistencias(futbolistas){
+    let jugadores = []
+      futbolistas.forEach(futbolista =>{
+        jugadores.push({
+          nombre: futbolista.nombre,
+          asistencias: futbolista.asistencias
+      })
+    })
+
+  jugadores.sort(function(a,b){
+    if(a.asistencias<b.asistencias){
+      return 1
+    }
+    if(a.asistencias>b.asistencias){
+      return -1
+    }
+    return 0
+  })
+  console.log(jugadores)
+  }
+
+  function contarTarjetas(futbolistas){
+  let equipos = []
+    futbolistas.forEach(futbolista =>{
+          equipos.push({
+            equipo: futbolista.equipo,
+            tarjetas: futbolista.tarjetaAmarilla + futbolista.tarjetaRoja
+          })
+    })
+  let indiceEliminar = 0
+  for(let i=0;i<equipos.length;i++){
+    for(let j=i+1;j<equipos.length;j++){
+      if(equipos[i].equipo === equipos[j].equipo){
+        equipos[i].tarjetas += equipos[j].tarjetas
+        equipos.splice(j,1)
+        j--
+      }
+    }
+  }
+  console.log(equipos)
+  }
+
+  function golesPromedio(futbolistas){  
+  let goles = []
+  futbolistas.forEach(futbolista=>{
+      goles.push({
+        nombre: futbolista.nombre,
+        promedioGoles: futbolista.goles / futbolista.partidosJugados
+    })
+  })
+
+  console.log(goles)
+
+  }
+  function mayorAsistencias(futbolistas){
+  let maxNombre = ""
+  let maxAsistencias = 0
+
+  futbolistas.forEach(futbolista=>{
+    if(futbolista.asistencias>maxAsistencias){
+      maxAsistencias = futbolista.asistencias
+      maxNombre = futbolista.nombre
+    }
+  })
+  console.log(maxNombre,maxAsistencias)
+  }
+
+  function listarPosicion(futbolistas){
+
+  let delanteros = []
+  let defensas = []
+  let porteros = []
+  let mediocampistas = []
+  futbolistas.forEach(futbolista=>{
+    switch (futbolista.posicion){
+      case "Delantero": 
+        delanteros.push(futbolista.nombre)
+        break
+      case "Portero": 
+        porteros.push(futbolista.nombre)
+        break
+      case "Defensa": 
+        defensas.push(futbolista.nombre)
+        break 
+      case "Mediocampista": 
+        mediocampistas.push(futbolista.nombre)
+        break
+      default:
+        break
+    }
+  })
+  let posiciones = ({
+    delanteros: delanteros,
+    defensas: defensas,
+    porteros: porteros,
+    mediocampistas: mediocampistas
+  })
+
+  console.log(posiciones)
+  }
+
