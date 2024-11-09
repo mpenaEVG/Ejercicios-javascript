97bd71d71869f96bf218e101084cde7b64081970
mpenaEVG <mauriciopenadominguez.guadalupe@alumnado.fundacionloyola.net>
2024-11-09 10:50:01 +0100

    Cambio en formato, figure para que se vean con  nombre

--- api-rickymorty/ejercicio.js
+++ api-rickymorty/ejercicio.js
@@ -5,18 +5,32 @@ fetch("https://rickandmortyapi.com/api/character")
       }
   })
 
-  .then(data=> data.results.map((item) => item.image).forEach(imagen => {
-  
-      const contenedor = document.getElementById('contenedor')
-      const img = document.createElement("img")
-      img.src = imagen
+  .then(data=> data.results.map((item) => { return {name : item.name , image: item.image}}).forEach(imagen => {
+        
+      const padre = document.getElementById('contenedor')
+
+      const personaje = document.createElement('figure')
+      personaje.style.display = 'inline-block'
+      personaje.style.margin = '10px'
+      personaje.style.textAlign = 'center'
+
+      const img = document.createElement('img')
+      img.src = imagen.image
       img.style.width = "200px"
       img.style.height = '200px'
-      img.style.margin = '10px'
-
-      contenedor.appendChild(img)
       
+      const titulo = document.createElement('figcaption')
+      titulo.textContent = imagen.name
+      titulo.style.marginTop = '5px'
+      titulo.style.fontSize = '16px'
+      titulo.style.color = 'green'
+      
+      personaje.appendChild(img)
+      personaje.appendChild(titulo)
+      padre.appendChild(personaje)
 }));
+
+  
 
 
 function desaparecer(){
